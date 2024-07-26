e41d4e09b7de6e2dec65e1426e2a8e53ffce7535
BarMaxx <mbarakhtanov@mail.ru>
2024-07-26 23:31:20 +0300

    0.0.3 Alpha
    - Изменены моды
    - Добавлен перевод мода Assembly Line Machines
    - Изменены квесты
    - Добавлены кастомные предметы для мода Mekanism
    - Изменено название окна игры
    - Прочие изменения

---
+++ kubejs/startup_scripts/new_items/for_mekanism.js
@@ -0,0 +1,12 @@
+StartupEvents.registry('item', event => {
+	
+	
+	event.create('osmium_catalyst').maxStackSize(16)
+	
+	
+	event.create('steel_catalyst').maxStackSize(16)
+	
+	
+	event.create('photocell')
+	
+})
